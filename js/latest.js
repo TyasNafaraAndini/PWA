9fa9b9b73f2e9abeabe905707cbc50c83e689f60
TyasNafaraAndini <145737177+TyasNafaraAndini@users.noreply.github.com>
2024-05-27 12:06:26 +0700

    Update latest.js

--- js/latest.js
+++ js/latest.js
@@ -33,7 +33,7 @@
 
     // Get Commit Data dari API Github
     function fetchCommits() {
-        var url = 'https://api.github.com/repos/TyasNafaraAndini/lampung/commits';
+        var url = 'https://api.github.com/repos/meizano/lampung/commits';
 
         fetch(url)
             .then(function (fetchResponse) {
